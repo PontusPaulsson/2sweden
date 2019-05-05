fd9bc2c52127d902534151d7461520e0c57d6bd2
Pontus Paulsson <pontus.p.paulsson@gmail.com>
2019-05-05 20:56:59 +0200

    Adding component Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Navbar = () => {
+    const links = ['Sök resor', 'Läs om eventet', 'Läs om våra orter', 'Se rekommendationer'];
+    return(
+        <nav>
+            <ul className="nav-list">
+                {links.map(link => <li className='nav-item'>{link}</li>)}
+            </ul>
+        </nav>
+    )
+};
