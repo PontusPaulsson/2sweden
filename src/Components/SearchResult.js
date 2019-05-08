ced0db3a83906d40592bd35e5672d5d08395406d
Ludde86 <bslbjorn@gmail.com>
2019-05-08 11:35:51 +0200

    added table searchResult

--- src/Components/SearchResult.js
+++ src/Components/SearchResult.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 //TODO Skicka in data till props och rendera ut detta i HTML.
-export const SearchResult = (props) => {
+export class SearchResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = [
+      {
+        transport: "",
+        time: "",
+        price: ""
+      }
+    ];
+  }
+  render() {
+    const data = [
+      {
+        transport: "",
+        time: "",
+        price: ""
+      }
+    ];
+
+    const columns = [
+      {
+        Header: "Färdmedel",
+        accessor: "transport" // String-based value accessors!
+      },
+      {
+        Header: "Tid",
+        accessor: "time"
+        // Cell: props => <span className="number">{props.value}</span> // Custom cell components!
+      },
+      {
+        accessor: "price", // Required because our accessor is not a string
+        Header: "Pris"
+      }
+    ];
+
     return (
-        <div><h1>Results</h1></div>
-    )
+      <ReactTable className="result-table" data={data} columns={columns} />
+    );
+  }
 }
