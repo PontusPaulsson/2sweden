92eaf83434fdcf4956f8ba339311c18219d08426
Erik <erik.magnus.olsson.sundh@gmail.com>
2019-05-14 15:02:54 +0200

    added 2 new dependecis * react-google-maps * decode-google-map-polyline

---
+++ src/Components/MapContainer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Component} from 'react';
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker, Polyline,
+} from "react-google-maps";
+
+class MapContainer extends Component {
+    render() {
+        const decodePolyline = require('decode-google-map-polyline');
+
+        var oslo = {lat: 59.91273, lng: 10.74609};
+        var sthml = {lat: 59.33258, lng: 18.0649};
+        var array = [];
+        array.push(oslo);
+        array.push(sthml);
+
+        // path: segment.path, adda i newSegment array.
+
+        // adda ny vid linje 124 i SearchResult.js {this.state.segmentTableToggle ? (
+        //                     <MapContainer children={this.state.segmentData}/>
+        //                 ) : null}
+
+        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+            <GoogleMap
+                defaultZoom={5}
+                defaultCenter={{lat: array[0].lat, lng: array[0].lng}}
+            >
+                <Polyline path={array}
+                          visible={true}
+                />
+
+            </GoogleMap>
+        ));
+        const url = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`;
+        return (
+            <MapWithAMarker
+                googleMapURL={url}
+                loadingElement={<div style={{height: `100%`}}/>}
+                containerElement={<div style={{height: `400px`}}/>}
+                mapElement={<div style={{height: `100%`}}/>}
+            />
+        )
+    }
+}
+
+export default MapContainer;
