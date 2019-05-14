6d4a09f5eab84dd2e4546fc2cbaec387122c82af
Arvid Melinder <43202769+Mellas84@users.noreply.github.com>
2019-05-14 15:51:43 +0200

    cornerd logo and spread out nav items

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -4,7 +4,8 @@ export const Title = () => {
   return (
     <div className="title-container">
       <img className="Logo" src={Logo} alt="Logo" />
-      <h1> 2Sweden OS 2024</h1>
+      <h1>2Sweden OS 2024</h1>
+      <div className="invis-margin" />
     </div>
   );
 };
