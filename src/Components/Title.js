e4d2d7931d8c56e2b883af807a9cbb8d1984d522
Arvid Melinder <43202769+Mellas84@users.noreply.github.com>
2019-05-14 15:06:14 +0200

    Final touches to css front page

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import Logo from "../img/Logo.png";
 export const Title = () => {
-  return <h1 className="title">2Sweden OS 2024</h1>;
+  return (
+    <div className="title-container">
+      <img className="Logo" src={Logo} alt="Logo" />
+      <h1> 2Sweden OS 2024</h1>
+    </div>
+  );
 };
