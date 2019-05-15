19e0ccb442483349241a7adb5e68110ab51bfa16
Arvid Melinder <43202769+Mellas84@users.noreply.github.com>
2019-05-15 12:47:18 +0200

    layout for result desktop

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import {Component} from 'react';
+import React from "react";
+import { Component } from "react";
 import {
-    withScriptjs,
-    withGoogleMap,
-    GoogleMap,
-    Marker, Polyline,
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  Polyline
 } from "react-google-maps";
 
 class MapContainer extends Component {
-    render() {
-        const decodePolyline = require('decode-google-map-polyline');
+  render() {
+    const decodePolyline = require("decode-google-map-polyline");
 
-        var oslo = {lat: 59.91273, lng: 10.74609}; // Hardcoded stuffs for some nice output
-        var sthml = {lat: 59.33258, lng: 18.0649};
-        var array = [];
-        array.push(oslo);
-        array.push(sthml);
-        console.log(this.props.children)
-        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
-            <GoogleMap
-                defaultZoom={5}
-                defaultCenter={{lat: array[0].lat, lng: array[0].lng}}
-            >
-                <Polyline path={array}
-                          visible={true}
-                />
-
-            </GoogleMap>
-        ));
-        const url = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`;
-        return (
-            <MapWithAMarker
-                googleMapURL={url}
-                loadingElement={<div style={{height: `100%`}}/>}
-                containerElement={<div style={{height: `400px`}}/>}
-                mapElement={<div style={{height: `100%`}}/>}
-            />
-        )
-    }
+    var oslo = { lat: 59.91273, lng: 10.74609 }; // Hardcoded stuffs for some nice output
+    var sthml = { lat: 59.33258, lng: 18.0649 };
+    var array = [];
+    array.push(oslo);
+    array.push(sthml);
+    console.log(this.props.children);
+    const MapWithAMarker = withScriptjs(
+      withGoogleMap(props => (
+        <GoogleMap
+          defaultZoom={5}
+          defaultCenter={{ lat: array[0].lat, lng: array[0].lng }}
+        >
+          <Polyline path={array} visible={true} />
+        </GoogleMap>
+      ))
+    );
+    const url = `https://maps.googleapis.com/maps/api/js?key=${
+      process.env.REACT_APP_GOOGLE_API_KEY
+    }&v=3.exp&libraries=geometry,drawing,places`;
+    return (
+      <MapWithAMarker
+        googleMapURL={url}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
 }
 
 export default MapContainer;
