adcac6408d311b1ae49bc878648c49255157112c
Erik <47929294+erikmolsson@users.noreply.github.com>
2019-05-16 11:02:51 +0200

    Revert "Google air"

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,12 @@
-import React from "react";
+import React from 'react';
 
 export const Navbar = () => {
-  const links = [
-    "Sök resor",
-    "Läs om eventet",
-    "Läs om våra orter",
-    "Se rekommendationer"
-  ];
-  return (
-    <nav>
-      <ul className="nav-list">
-        {links.map(link => (
-          <li className="nav-item" key={link}>
-            {link}
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+    const links = ['Sök resor', 'Läs om eventet', 'Läs om våra orter', 'Se rekommendationer'];
+    return(
+        <nav>
+            <ul className="nav-list">
+                {links.map(link => <li className='nav-item' key={link}>{link}</li>)}
+            </ul>
+        </nav>
+    )
+}
