c5329e488f834fee6e85b92a494ebff4898794fe
GremlinAtTheDisco <skyesme@gmail.com>
2019-05-20 10:26:40 +0200

    added navbar test

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Navbar = () => {
     const links = ['Sök resor', 'Läs om eventet', 'Läs om våra orter', 'Se rekommendationer'];
     return(
-        <nav>
+        <nav data-test ='nav-bar-container'>
             <ul className="nav-list">
                 {links.map(link => <li className='nav-item' key={link}>{link}</li>)}
             </ul>
