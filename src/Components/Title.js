cab8f6c51303daaa58f0a33488b0ab31b8d16764
Arvid Melinder <43202769+Mellas84@users.noreply.github.com>
2019-05-21 13:31:02 +0200

    change header front and mobile + nav mobile

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -2,10 +2,16 @@ import React from "react";
 import Logo from "../img/Logo.png";
 export const Title = () => {
   return (
-    <div className="title-container">
-      <img className="Logo" src={Logo} alt="Logo" />
-      <h1>2Sweden OS 2024</h1>
+    <h1 className="title-container">
+      <img className="desktop-logo" src={Logo} alt="Logo" />
+      <div className="header-container">
+        <span>2Sweden</span>
+        <span className="title-logoitem">
+          <img className="logo" src={Logo} alt="Logo" />S
+        </span>
+        <span>2024</span>
+      </div>
       <div className="invis-margin" />
-    </div>
+    </h1>
   );
 };
